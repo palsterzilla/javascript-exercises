a5a5286fc5ed08892b945bb452833e32f9d44285
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-20 21:15:31 +0700

    Implement function to remove given item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeFromArray) {
+  let newArray = [];
 
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] !== removeFromArray) {
+        newArray.push(array[i]);
+      }
+    }
+
+  return newArray;
 };
 
 // Do not edit below this line
