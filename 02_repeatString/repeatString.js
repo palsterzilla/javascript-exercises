cea8c181dff37a2ecf5af24f0a67c8a36bce40e0
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-20 20:43:07 +0700

    Implement return "ERROR" if given num is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,14 @@
 const repeatString = function(string, num) {
   let finalString = "";
-  for (let i = 0; i < num; i++) {
-    finalString += string;  
+
+  if (num < 0) {
+    finalString = "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      finalString += string;  
+    }
   }
+
   return finalString;
 };
 
