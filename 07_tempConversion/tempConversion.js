9c3a2d3aa780c97af0969ef8a3c3df60f4d1081e
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-22 18:17:53 +0700

    Return value converted from celcius degree to fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  let celciusVal = (val - 32) * 5/9
+
+  return celciusVal
 };
 
 const convertToFahrenheit = function() {
