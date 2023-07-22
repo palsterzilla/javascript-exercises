c3f8074560756f7b31cab86317dfd225312532ea
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-22 14:32:24 +0700

    Implement return 'ERROR' if parameter type is not number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,10 @@
 const sumAll = function(...args) {
-  if (args[1] < args[0]) {
-    args.sort()
-  } else if (args.some(x => x < 0)) {
+  if (args.some(x => x < 0 || typeof x != "number")) {
     return "ERROR"
+  } else if (args[1] < args[0]) {
+    args.sort()
   }
-  
+
   let finalNum = 0;
 
   for (let i = args[0]; i <= args[1]; i++) {
