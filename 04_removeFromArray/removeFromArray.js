7a4f4fba3db5a6fbafc72d2fb23869586ee46e7f
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-22 13:42:46 +0700

    Refactor function logic

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,6 @@
 const removeFromArray = function(array, ...removeFromArray) {
-  let newArray = [];
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < removeFromArray.length; j++) {
-      if (array[i] !== removeFromArray[j]) {
-        newArray.push(array[i]);
-      }
-    }
-  }
-
-  return newArray;
+  return array.filter(x => !removeFromArray.includes(x))
 };
 
 // Do not edit below this line
