a2612623850d69e3e02e0dc3fda2fc1354d624d6
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-22 11:32:45 +0700

    Replace using rest parameter in the function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,13 @@
-const removeFromArray = function(array, removeFromArray) {
+const removeFromArray = function(array, ...removeFromArray) {
   let newArray = [];
 
   for (let i = 0; i < array.length; i++) {
-      if (array[i] !== removeFromArray) {
+    for (let j = 0; j < removeFromArray.length; j++) {
+      if (array[i] !== removeFromArray[j]) {
         newArray.push(array[i]);
       }
     }
+  }
 
   return newArray;
 };
