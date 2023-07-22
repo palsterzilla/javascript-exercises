b1acbbc0049bdb65ec03baabfb44506d7f30efe0
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-22 14:03:15 +0700

    Implement logic return value from given parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  let finalNum = 0;
 
+  for (let i = args[0]; i <= args[1]; i++) {
+    finalNum += i;
+  }
+  
+  return finalNum
 };
 
 // Do not edit below this line
