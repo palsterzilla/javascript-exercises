be7b327375fcf670552a91a1c5d5939eb5bc6d1b
Firyal Ghassari <naufal.firyal@gmail.com>
2023-08-12 19:48:26 +0700

    Exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let number = num;
+  if (num != Number) {
+    number = Number(num)
+  }
 
+  let n1 = 0, n2 = 1, nextTerm;
+  if (number === 1 || number === 0) {
+    return 1;
+  } else if (number < 0) {
+    return "OOPS"
+  } else {
+    for (let i = 2; i <= number; i++) {
+     nextTerm = n1 + n2;
+     n1 = n2
+     n2 = nextTerm
+    }
+  }
+
+  return nextTerm
 };
 
 // Do not edit below this line
