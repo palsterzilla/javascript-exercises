ed7d572e7d25b87f8473ec3949a704ed3e1bbf5a
Firyal Ghassari <naufal.firyal@gmail.com>
2023-08-12 19:06:18 +0700

    Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newStr = string
+                .replace(/[\W\s]+/g,"")
+                .toLowerCase()
+                
+  let revStr = newStr
+                .split("")
+                .reverse()
+                .join("")
+  
+  if (newStr === revStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
